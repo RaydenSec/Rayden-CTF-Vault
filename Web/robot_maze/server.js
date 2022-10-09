17398ba0e76e9f89768fe0b63bdb6d01e0e7cc6f
Rayden <raydenwang883@gmail.com>
2022-10-10 03:36:55 +1100

    Added robot maze challenge

---
+++ Web/robot_maze/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000; 
+
+app.use(express.static("views"))
+
+app.set("view engine", "ejs"); 
+app.get('/', (req, res) => res.render("index")); 
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
