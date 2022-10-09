6833e32ce0891bdf43b3b1ce8368fd8b5ce44668
Rayden <raydenwang883@gmail.com>
2022-10-10 02:57:07 +1100

    Modified yarn.c code to fix error and make the logic more simple

---
+++ Reversing/cat_ball/yarn.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+void validator(char user_input[]);  
+
+int main(void) {
+
+    char user_input[10];    // includes \0 at the end
+
+    printf("Enter Password: "); 
+    fgets(user_input, 10, stdin); 
+
+    validator(user_input); 
+
+    return 0; 
+}
+
+void validator(char user_input[]) {
+
+    char password[] = "u2&igqGui";  
+
+    user_input[strcspn(user_input, "\n")] = '\0'; // strcspn checks if the string contains the given char, and returns the index of it. we're setting the newline to the end of the string
+    // why is "\n" an int when '\n'? isn't it a char? 
+
+    if (strcmp(password, user_input) == 0) {    // taking new line character at the end
+        printf("(Congrats! Flag is TEST0{N0t_iT_;D})\n");  
+        return; 
+    } 
+    
+    printf("Wrong ;)\n"); 
+    printf("Try again: "); 
+    fgets(user_input, 10, stdin); 
+
+    validator(user_input); 
+}
+
+// echo "hi" | md5sum
+// cp -a "folder" "target" (copy folder and all contents to location)
+// rm -r "folder" (remove folder and all contents)
